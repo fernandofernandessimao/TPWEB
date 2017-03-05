13baef26a2d7f9aad721983eada7e453ec48f775
JoaoBizarro <joao1bizarro@gmail.com>
2017-03-05 20:03:38 +0000

    Schedule Class

---
+++ src/java/code/BackgroundJobManager.java
@@ -0,0 +1,38 @@
+package code;
+
+import java.util.Date;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+
+@Singleton
+public class BackgroundJobManager {
+
+    @EJB
+    private ItemFacadeLocal iFacade;
+    @EJB
+    private UtilizadorFacadeLocal uFacade;
+
+   public TUtilizador getUser(String username) {
+        List<TUtilizador> l = uFacade.getAll();
+        for (TUtilizador u : l) {
+            if (u.getUsername().equals(username)) {
+                return u;
+            }
+        }
+        return null;
+    }
+    
+    @Schedule(hour="0", minute="0", second="0", persistent=false)
+    public void DailyJob() {
+        Date d = new Date();
+        List<TItem> l = iFacade.getAll();
+        for(int i=0;i<l.size();i++){
+            if (!d.before(l.get(i).getPrazo())) {
+                iFacade.setConcluido(l.get(i));
+                iFacade.setComprador(getUser(l.get(i).getBidder()), l.get(i));
+            }
+        }
+    }
+}
